e9e87276c4cc7d5f6f7f410be1a601d7ec39a24b
iiamsona <sona22hakobyan@gmail.com>
2025-04-01 16:16:36 +0400

    feat/table column added

---
+++ src/components/atoms/mainTable/MainTable.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="main_table">
+       <TableColumn />
+    </div>
+</template>
+
+<script setup>
+import TableColumn from '../tableColumn/TableColumn.vue'
+</script>
+
+<style lang="scss" scoped>
+  @import "./index.scss";
+</style>
---
+++ src/components/atoms/tableColumn/TableColumn.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="table_column">
+        <div>
+            ID
+        </div>
+        <div>
+            Name
+        </div>
+        <div>
+            Address
+        </div>
+        <div>
+            Phone
+        </div>
+        <div>
+            Email
+        </div>
+        <div>
+            Website
+        </div>
+        <div>
+            Organization type
+        </div>
+        <div>
+            Status
+        </div>
+        <div>
+            Terminal
+        </div>
+        <div>
+            Action
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+  @import "./index.scss";
+</style>
