cfa2f638e2f5ca74308f7c30fcdf452cf7460c35
iiamsona <sona22hakobyan@gmail.com>
2025-04-02 17:45:28 +0400

    feat/status button functionality done

--- src/components/atoms/statusButton/StatusButton.vue
+++ src/components/atoms/statusButton/StatusButton.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>
+  <div class="status-button-container">
     <div v-if = "!status" class="status-button-inactive">
       <p class="status-button_text-inactive">Inactive</p>
-      <button @click=changeStatus() class="status-button_btn"><img :src="arrow"></img></button>
+      <button @click=openStatus() class="status-button_btn"><img :src="arrow"></img></button>
     </div>
     <div v-else class="status-button-active">
-        <p class="status-button_text-active">Active</p>
-          <button @click=changeStatus() class="status-button_btn"><img :src="arrow"></img></button>
-      </div>
+      <p class="status-button_text-active">Active</p>
+      <button @click=openStatus() class="status-button_btn"><img :src="arrow"></img></button>
+    </div>
+    <div v-if="open" class="change-status-container">
+      <p :class="status ? 'change-status-selected' : 'change-status'" @click=changeActiveStatus()>Active</p>
+      <p :class="status ? 'change-status' : 'change-status-selected'" @click=changeInactiveStatus()>Inactive</p>
+    </div>
   </div>
 </template>
 
@@ -23,18 +27,28 @@
     status: {
       type: Boolean,
       default: true
+    },
+    open:{
+      type:Boolean,
+      default: false
     }
   },
   data() {
     return {
         arrow,
-        status
+        status,
+        open
     }
   },
   methods:{
-    changeStatus(){
-      this.status = !this.status;
-      console.log(this.status);
+    changeActiveStatus(){
+      this.status = true;
+    },
+    changeInactiveStatus(){
+      this.status = false;
+    },
+    openStatus(){
+      this.open = !this.open;
     }
   }
 }
