0b25602d3d276e1c846b64954a4dbed684913aa6
iiamsona <sona22hakobyan@gmail.com>
2025-04-07 11:15:00 +0400

    feat/filter button added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 
 <template>
+    <CustomizeButton/>
     <MainTable />
 </template>
  
 
 <script setup>
+import CustomizeButton from './components/atoms/customizeButton/CustomizeButton.vue';
 import MainTable from './components/atoms/mainTable/MainTable.vue';
 </script>
--- src/components/atoms/customizeButton/customizeButton.vue
+++ src/components/atoms/customizeButton/customizeButton.vue
@@ -1,17 +1,28 @@
 <template>
     <div class="customize-button">
-        <button>
+        <button @click="open = !open" class="customize-button-btn">
+            <p>Filter</p>
             <img :src="more"></img>
         </button>
-        <div>
-            
+        <div class="customize-button-container">
+            <div v-if="open === true" v-for="column in columnsArray" :key="column.key" class="customize-button-div">
+                <div class="customize-button-column">
+                    <input type="checkbox" :value="column.key" v-model="column.visible" />
+                    <p>{{column.label}}</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import more from '../../../assets/more.svg'
+import { columnsArray } from "../../../data/index.js";
+
+const open = ref(false);
 </script>
+
 
 <style lang="scss" scoped>
   @import "./index.scss";
